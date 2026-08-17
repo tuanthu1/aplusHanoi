f74842afc82e53cf09ea51ae7fb22d26bc387e00
Do Thang <110244660+tuanthu1@users.noreply.github.com>
2026-08-17 16:39:04 +0700

    Update axios baseURL to include '/api'

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const axiosClient = axios.create({
-  baseURL: "https://aplushn-api.onrender.com",
+  baseURL: "https://aplushn-api.onrender.com/api",
   headers: {
     'Content-Type': 'application/json',
   },
