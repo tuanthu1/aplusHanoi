2792ce279e232b6850f13327c5f36127c1b058c8
Do Thang <110244660+tuanthu1@users.noreply.github.com>
2026-09-02 15:39:20 +0700

    Delete server directory

--- server/routes/eventRoutes.js
+++
@@ -1,8 +0,0 @@
-// FILE: routes/eventRoutes.js
-const express = require('express');
-const router = express.Router();
-const EventController = require('../controllers/eventController');
-
-router.get('/', EventController.getAllEvents);
-
-module.exports = router;
